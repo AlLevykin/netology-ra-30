7224eb3673935150c5ff6dbf3f51601af874c06c
AlLevykin <a.a.levykin@gmail.com>
2021-07-01 01:13:34 +0500

    Инициализация репозитория.

---
+++ homework1-components/src/components/App/index.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="background-element">
+      </div>
+    </div>
+  );
+}
+
+export default App;
