4e9c181405e8db5b8a301e5c2570974f29cd395b
AlLevykin <a.a.levykin@gmail.com>
2021-07-04 10:44:15 +0500

    IconSwitch

--- homework2-events-state/store-app/src/components/Store/IconSwitch/index.jsx
+++ homework2-events-state/store-app/src/components/Store/IconSwitch/index.jsx
@@ -1,7 +1,15 @@
-function IconSwitch () {
+import PropTypes from 'prop-types';
+
+function IconSwitch({icon, onClick}) {
+
     return (
-        <h1>IconSwitch</h1>
+        <button onClick={onClick}><span className="material-icons">{icon}</span></button>
     );
+}
+
+IconSwitch.propTypes = {
+    icon: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired
 }
 
 export default IconSwitch;
--- homework2-events-state/store-app/src/components/Store/index.jsx
+++ homework2-events-state/store-app/src/components/Store/index.jsx
@@ -1,13 +1,37 @@
+import { useState } from 'react';
 import IconSwitch from './IconSwitch';
 import ListView from './ListView';
 import './Store.css';
 
-function Store() {
+function Store({ caption, products }) {
+
+    const [viewType, setState] = useState(0);
+
+    const viewTypes = [
+        {
+            name: 'cards',
+            icon: 'view_module'
+        },
+        {
+            name: 'tiles',
+            icon: 'view_comfy'
+        },
+        {
+            name: 'list',
+            icon: 'view_list'
+        }];
+
+    const changeViewType = () => {
+        setState(
+            viewType < viewTypes.length - 1 ? viewType + 1 : 0
+        );
+    }
+
     return (
         <div>
-            <h1>Store</h1>
-            <IconSwitch />
-            <ListView />
+            <h1>{caption}</h1>
+            <IconSwitch icon={viewTypes[viewType].icon} onClick={changeViewType} />
+            <ListView products={products} viewType={viewTypes[viewType].name}/>
         </div>
     );
 }
