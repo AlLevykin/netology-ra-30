8f932cecb91a235ea6d089608b41d5c2ddff3e98
AlLevykin <a.a.levykin@gmail.com>
2021-07-06 06:16:05 +0500

    Добавлен стиль shopitem_caption

--- homework2-events-state/store-app/src/components/Store/ShopItem/index.jsx
+++ homework2-events-state/store-app/src/components/Store/ShopItem/index.jsx
@@ -4,7 +4,7 @@ function ShopItem({ viewType, product }) {
     return (
         <div className={`shopitem shopitem_${viewType}`}>
             <div>
-                <h1 className={`shopitem_caption_${viewType}`}>{product.name}</h1>
+                <h1 className={`shopitem_caption shopitem_caption_${viewType}`}>{product.name}</h1>
                 <h2 className={`shopitem_color shopitem_color_${viewType}`} style={{color: product.color}}>{product.color}</h2>
             </div>
             <img className="shopitem" src={product.img} alt={product.name} title={`${product.name}, color: ${product.color}`}></img>
