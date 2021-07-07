be760f82a17cc956dd18a8c3bdceb3c6628e8ff0
AlLevykin <a.a.levykin@gmail.com>
2021-07-08 04:27:48 +0500

    propTypes в компоненте ListingItem

--- homework3-props/etsy-app/src/components/Listing/index.jsx
+++ homework3-props/etsy-app/src/components/Listing/index.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import ListingItem from './ListingItem/ListingItem';
 import './Listing.css';
 
@@ -12,6 +13,14 @@ function Listing({ brand, items }) {
             </div>
         </>
     );
+}
+
+Listing.propTypes = {
+    brand: PropTypes.string.isRequired,
+    items: PropTypes.arrayOf(PropTypes.shape({
+        state: PropTypes.string.isRequired,
+        listing_id: PropTypes.number.isRequired
+    }))
 }
 
 export default Listing;
