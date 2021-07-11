cfd33ea1814cc3ebd7d82c40630b56085c4196a6
AlLevykin <a.a.levykin@gmail.com>
2021-07-12 01:06:21 +0500

    ColorConverter improved

--- homework4-forms/hex2rgb-app/src/components/ColorConverter/index.jsx
+++ homework4-forms/hex2rgb-app/src/components/ColorConverter/index.jsx
@@ -17,23 +17,41 @@ const HEXtoRGB = (input) => {
     let green = parseInt(result[2], 16);
     let blue = parseInt(result[3], 16);
 
-    return { valid: true, rgb: `rgb(${red}, ${green}, ${blue})`, darken: `rgb(${darken(red)}, ${darken(green)}, ${darken(blue)})`, hex: input };
+    return {
+      value: `rgb(${red}, ${green}, ${blue})`,
+      rgb: `rgb(${red}, ${green}, ${blue})`,
+      darken: `rgb(${darken(red)}, ${darken(green)}, ${darken(blue)})`,
+      hex: input
+    };
   } else {
-    return { valid: false, rgb: 'rgb(255,0,0)', darken: 'rgb(178,0,0)', hex: input };
+    return {
+      value: 'Error!',
+      rgb: 'rgb(255,0,0)',
+      darken: 'rgb(178,0,0)',
+      hex: input
+    };
   }
 }
 
 function ColorConverter() {
   const [color, setColor] = useState(HEXtoRGB('#ffffff'));
 
-  const onInput = (e) => setColor(HEXtoRGB(e.target.value));
+  const handleHEXChange = (event) => setColor(
+    HEXtoRGB(event.target.value)
+  );
 
   return (
     <div className="ColorConverter-Container" style={{ background: color.rgb }}>
       <div className="ColorConverter-Form">
-        <input onChange={onInput} value={color.hex} />
+        <input
+          type="text"
+          placeholder="Color HEX code"
+          maxLength="7"
+          onChange={handleHEXChange}
+          value={color.hex}
+        />
         <div className="ColorConverter-Label" style={{ background: color.darken }}>
-          {color.valid ? color.rgb : 'Ошибка!'}
+          {color.value}
         </div>
       </div>
     </div>
