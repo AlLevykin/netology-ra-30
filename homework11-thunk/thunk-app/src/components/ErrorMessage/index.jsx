077838f363bc875749284f7ab6afab7e4e111eb3
Aleksey Levykin <a.a.levykin@gmail.com>
2021-08-10 17:02:18 +0300

    Update index.jsx

--- homework11-thunk/thunk-app/src/components/ErrorMessage/index.jsx
+++ homework11-thunk/thunk-app/src/components/ErrorMessage/index.jsx
@@ -1,6 +1,21 @@
-const ErrorMessage = ({ text }) =>
+import { useDispatch } from 'react-redux';
+import { ArrowClockwise } from 'react-bootstrap-icons';
+import { fetchServices } from '../../actions/actionCreators';
+
+const ErrorMessage = ({ text }) => {
+
+    const dispatch = useDispatch();
+
+    const handleClick = () => dispatch(fetchServices());
+
+    return (
     <div className="alert alert-danger my-3" role="alert">
+        <button className="btn btn-danger mx-3" type="button" onClick={handleClick}>
+            <ArrowClockwise />
+        </button>
         {text}
     </div>
+    );
+}
 
 export default ErrorMessage;
